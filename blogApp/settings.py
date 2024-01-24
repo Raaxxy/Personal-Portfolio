a5d7e60f7759acfdb90ffdc667b0c31fd6f92f29
Raaxxy <rahularyan0197@gmail.com>
2024-01-24 07:28:31 +0530

    added AWS S3 and AWS RDS for database

--- blogApp/settings.py
+++ blogApp/settings.py
@@ -94,23 +94,33 @@ WSGI_APPLICATION = 'blogApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'railway',                      
-#         'USER': 'postgres',
-#         'PASSWORD': 'cqGiSNnstwaz2lyPFb4N',
-#         'HOST': 'containers-us-west-69.railway.app',
-#         'PORT': '6440',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+
+# Amazon RDS PostgreSQL database - markup
+
+DATABASES = {
+
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql',
+
+        'NAME': 'portfolio_db',
+
+        'USER': 'raxxy',
+
+        'PASSWORD': env('DB_PASSWORD'),
+
+        'HOST': 'portfolio.cx2egukc8ztl.ap-south-1.rds.amazonaws.com',
+
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
